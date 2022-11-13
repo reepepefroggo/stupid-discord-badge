89641662b9dff0a1dc3f4175b561c43c133cac82
kirb <49120903+reepepefroggo@users.noreply.github.com>
2022-11-13 09:47:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ async function init(token) {
     // Register the stupid ass command
     stupidAssBot.on('ready', async () => {
         await stupidAssBot.bulkEditCommands([{
-            name: 'lol',
-            description: 'I hate discord so much you cannot believe it',
+            name: 'glung',
+            description: 'Glungus will start glungin',
             type: 1,
         }])
         console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
     // Stupid ass interaction creation event
     stupidAssBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'lol') {
+        if (interaction?.data?.name === 'glung') {
             await interaction.createMessage({
-                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
+                content: 'https://tenor.com/view/synapse-electrical-neuron-gif-15531932'
             })
             console.log('Self destructing...')
             process.exit(0)
